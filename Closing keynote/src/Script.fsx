f047813b5433d6ad94ec19cc8ba44cc4f8c53c32
Luigi Berrettini <luigi.berrettini@gmail.com>
2015-03-31 22:33:56 +0200

    Add keynotes

---
+++ Closing keynote/src/Script.fsx
@@ -0,0 +1,8 @@
+﻿let genRandomNumbers count min max =
+      let rnd = System.Random()
+      List.init count (fun _ -> rnd.Next (min, max))
+  
+let shortList = genRandomNumbers 10 1 290
+
+printfn "\n\nThe winner is %i\n\n"
+  (shortList.Item 0)
